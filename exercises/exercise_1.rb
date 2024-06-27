e8e8b106eeb4b761cdbf0dcd47b7a5cab5ba8589
WishNavy <du.vyshu@gmail.com>
2024-06-26 19:51:20 -0700

    finish exercise 3

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,13 +5,8 @@ puts "----------"
 
 # Your code goes below here ...
 
-Burnaby = Store.create()
-Richmond = Store.create()
-Gastown = Store.create()
+Burnaby = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Richmond = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Gastown = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
-
-@store1 = Store.find(1)
-@store2 = Store.find(2)
-
-@store1.name = 'MetroTown'
